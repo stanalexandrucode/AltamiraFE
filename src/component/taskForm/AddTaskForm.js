72fd4d71892d2303a77f0d4f0c9bb19b3121287a
stanalexandru <stanalexandrucode@gmail.com>
2021-07-07 13:21:21 +0300

    Created routes and Task formControl

--- src/component/taskForm/AddTaskForm.js
+++ src/component/taskForm/AddTaskForm.js
@@ -1,53 +1,78 @@
 import "bootstrap/dist/css/bootstrap.css";
 import {Form} from 'react-bootstrap';
 import './taskForm.css'
-import {DateRangePickerComponent} from '@syncfusion/ej2-react-calendars';
+import axios from "axios";
+import {useState} from "react";
+import {useHistory} from 'react-router-dom';
 
 const AddTaskForm = () => {
-    const startValue: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 14);
-    const endValue: Date = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 15);
-    const minDate: Date = new Date(new Date().getFullYear(), new Date().getMonth(), 8);
-    // const maxDate: Date = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 20);
+
+    const [tipTask, setTipTask] = useState();
+    const [name, setName] = useState();
+    const [durataLimita, setDurataLimita] = useState();
+    const [durataEstimata, setdurataEstimata] = useState();
+
+    const history = useHistory();
+
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear()
+    today = yyyy + '-' + mm + '-' + dd;
+
+
+    const handleAdd = async (e) => {
+        e.preventDefault()
+        let response = await axios({
+            method: 'post',
+            url: 'http://localhost:8080/task/add',
+            data: {tipTask, name, durataLimita, durataEstimata},
+        }).catch((err) => console.log('Error:', err));
+        if (response) {
+            history.push("/")
+        }
+    }
 
     return (
         <>
+            <div className="form-group col-xs-10 col-sm-4 col-md-4 col-lg-4">
                 <Form>
-                    <Form.Group controlId="exampleForm.ControlSelect1">
+                    <Form.Group controlId="exampleForm.ControlSelect1"
+                                onChange={e => setTipTask(e.target.value)}>
                         <Form.Label>Tip Task</Form.Label>
-                        <Form.Control as="select">
+                        <Form.Control as="select"
+                        >
                             <option>Work</option>
                             <option>Home</option>
                             <option>Hobby</option>
                         </Form.Control>
                     </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
+                    <Form.Group controlId="formBasicPassword"
+                                onChange={e => setName(e.target.value)}>
                         <Form.Label>Name</Form.Label>
                         <Form.Control type="text" placeholder="name"/>
                     </Form.Group>
+
                     <Form.Group controlId="exampleForm.ControlInput1">
-                        <Form.Label>Data Limita,</Form.Label>
-                        <DateRangePickerComponent
-                            placeholder="Enter Date Range"
-                            // startDate={startValue}
-                            endDate={endValue}
-                            min={minDate}
-                            // max={maxDate}
+                        <label>Data limita</label>
+                        <input
+                            onChange={e => setDurataLimita(e.target.value)}
+                            className="form-control"
+                            type="date" id="dev_end_date"
+                            min={today}
                         />
                     </Form.Group>
-                    <Form.Group controlId="exampleForm.ControlInput1">
-                        <Form.Label>Durata Estimata</Form.Label>
-                        <DateRangePickerComponent
-                            placeholder="Enter Date Range"
-                            // startDate={startValue}
-                            endDate={endValue}
-                            min={minDate}
-                            // max={maxDate}
-                            minDays={1}
-                            // maxDays={5}
-                        />
+                    <Form.Group controlId="formBasicPassword"
+                    >
+                        <Form.Label>Durata limita in zile</Form.Label>
+                        <Form.Control
+                            onChange={e => setdurataEstimata(e.target.value)}
+                            type="number" min="0" placeholder="Durata"/>
                     </Form.Group>
-                    <button className="btn btn-primary">Save task</button>
+
+                    <button className="btn btn-primary" onClick={e => handleAdd(e)}>Save task</button>
                 </Form>
+            </div>
         </>
     );
 }
